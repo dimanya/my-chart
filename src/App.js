33fcf1d26923aab4bb02ad970c85fbdd86591926
dimanya <dimanya35@gmail.com>
2021-07-23 11:04:51 +0500

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { scaleLinear, scaleTime, timeFormat, extent } from 'd3';
+import { useData } from './useData';
+import { AxisBottom } from './AxisBottom';
+import { AxisLeft } from './AxisLeft';
+import { Marks } from './Marks';
 
-function App() {
+const width = 960;
+const height = 500;
+const margin = { top: 20, right: 30, bottom: 65, left: 90 };
+const xAxisLabelOffset = 50;
+const yAxisLabelOffset = 45;
+
+const App = () => {
+  const data = useData();
+
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
+  const xValue = d => d.timestamp;
+  const xAxisLabel = 'Time';
+
+  const yValue = d => d.temperature;
+  const yAxisLabel = 'Temperature';
+
+  const xAxisTickFormat = timeFormat('%a');
+
+  const xScale = scaleTime()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([innerHeight, 0])
+    .nice();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+          tickOffset={7}
+        />
+        <text
+          className="axis-label"
+          textAnchor="middle"
+          transform={`translate(${-yAxisLabelOffset},${innerHeight /
+            2}) rotate(-90)`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {yAxisLabel}
+        </text>
+        <AxisLeft yScale={yScale} innerWidth={innerWidth} tickOffset={7} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          {xAxisLabel}
+        </text>
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
+          circleRadius={3}
+        />
+      </g>
+    </svg>
   );
-}
+};
 
 export default App;
