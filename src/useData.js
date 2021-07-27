47446e8342e5021e3f31fa8a191132eca8b392bc
dimanya <dimanya35@gmail.com>
2021-07-27 23:08:53 +0500

    full

--- src/useData.js
+++ src/useData.js
@@ -1,25 +1,3 @@
-//import { useState, useEffect } from 'react';
-//import { csv } from 'd3';
-
-//const csvUrl =
-  //'https://gist.githubusercontent.com/curran/90240a6d88bdb1411467b21ea0769029/raw/7d4c3914cc6a29a7f5165f7d5d82b735d97bcfe4/week_temperature_sf.csv';
-
-//export const useData = () => {
-  //const [data, setData] = useState(null);
-
-  //useEffect(() => {
-    //const row = d => {
-      //d.temperature = +d.temperature;
-      //d.timestamp = new Date(d.timestamp);
-      //return d;
-    //};
-    //csv(csvUrl, row).then(setData);
-  //}, []);
-  //console.log(data);
-  //return data;
-//};
-
-
 import { useState, useEffect } from 'react';
 
 export const useData = () => {
@@ -30,11 +8,23 @@ export const useData = () => {
    fetch('https://gist.githubusercontent.com/dimanya/abd990644f41f906a3676bf1bebefa5e/raw/5e5bb4746d214a83fc8e5c2784a2c349ea808b67/data.json')
       .then(response => response.json())
       .then(data => {
-        const measurementOD = data.filter(event => {
-          return event.measurementOD === 'measurementOD'
-        })
-        con
-        return measurementOD;
+        let measurementOD = data.filter(event => {
+          return event.measurementOD
+        });
+
+        let res = [];
+
+        // eslint-disable-next-line array-callback-return
+        measurementOD.map((item) => {
+          let measurementO = item.measurementOD;
+          let timestampO = new Date(item.timestamp/1e4);
+          const result = {measurementOD:measurementO, timestamp:timestampO};
+          res.push(result);
+        });
+
+        console.log(res);
+
+        return res;
       })
       .then(setDataChart)
   }, []);
