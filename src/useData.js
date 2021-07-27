6466f78510d10a85edddd80e689ef66936104c7b
dimanya <dimanya35@gmail.com>
2021-07-27 15:49:06 +0200

    a

--- src/useData.js
+++ src/useData.js
@@ -21,24 +21,23 @@
 
 
 import { useState, useEffect } from 'react';
-import { json } from 'd3';
-
-const jsonUrl =
-  'https://gist.githubusercontent.com/dimanya/abd990644f41f906a3676bf1bebefa5e/raw/5e5bb4746d214a83fc8e5c2784a2c349ea808b67/data.json';
 
 export const useData = () => {
 
-    const [data, setData] = useState(null);
+    const [dataChart, setDataChart] = useState(null);
 
      useEffect(() => {
-   const row = d => {
-      d.measurementOD = +d.measurementOD;
-      d.timestamp = new Date(d.timestamp/1e4);
-      
-      return d;
-    };
-    json(jsonUrl, row).then(setData);
+   fetch('https://gist.githubusercontent.com/dimanya/abd990644f41f906a3676bf1bebefa5e/raw/5e5bb4746d214a83fc8e5c2784a2c349ea808b67/data.json')
+      .then(response => response.json())
+      .then(data => {
+        const measurementOD = data.filter(event => {
+          return event.measurementOD === 'measurementOD'
+        })
+        con
+        return measurementOD;
+      })
+      .then(setDataChart)
   }, []);
-  console.log(data);
-    return data;
+  console.log(dataChart);
+    return dataChart;
 };
